e6354027125a33a07a262a076935638dcc1350bc
Geocube333 <Geocube333@gmail.com>
2019-04-13 21:34:06 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ function hex2dec() {
   
   for(i = 48; 48<i<90; i++){
     charCodeAt(i);
-    document.getElementById('home-button').innerHTML = x;
+    document.getElementById('home-button').innerHTML = i;
   };
   
 };
